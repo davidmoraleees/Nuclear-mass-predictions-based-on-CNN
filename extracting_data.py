a23cab05327045f082d3b17869525864edf668d0
davidmoraleees <davidmoralesdeleon@gmail.com>
2024-09-26 18:14:52 +0200

    Plot changed

--- extracting_data.py
+++ extracting_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 df = pd.read_fwf(
     'mass_1.mas20.txt',
@@ -18,16 +19,16 @@ df['Z'] = df['Z'].astype(int)
 df['A'] = df['A'].astype(int)
 df['bind_ene'] = df['bind_ene'].astype(float)
 
-# Càlculs amb el model teòric
+# Theoretical model calculations
 av = 15.8 #MeV
 aS = 18.3
 ac = 0.66
 aA = 23.2
 ap = 11.2
 
-df['delta'] = np.where((df['Z'] % 2 == 0) & (df['N'] % 2 == 0), -1,  # Z parell i N parell
-                np.where(df['A'] % 2 == 1, 0,  # A senar
-                np.where((df['Z'] % 2 == 1) & (df['N'] % 2 == 1), 1, np.nan)))  # Z senar i N senar
+df['delta'] = np.where((df['Z'] % 2 == 0) & (df['N'] % 2 == 0), -1,  # Z even and N even
+                np.where(df['A'] % 2 == 1, 0,  # A odd
+                np.where((df['Z'] % 2 == 1) & (df['N'] % 2 == 1), 1, np.nan)))  # Z odd and N odd
 
 df['delta'] = df['delta'].astype(int)
 
@@ -36,27 +37,35 @@ N = df['N']
 A = df['A']
 delta = df['delta']
 
+df = df[(df['N'] >= 8) & (df['N'] < 180) & (df['Z'] >= 8) & (df['Z'] < 120)] #Restrictions 
+
 bind_ene_teo = (av*A-aS*A**(2/3)-ac*Z**2*A**(-1/3)-aA*(A-2*Z)**2/A-ap*delta*A**(-1/2))/A*1000 #keV
 
 df['bind_ene_teo'] = bind_ene_teo
-df['Diferencia_bind_ene'] = df['bind_ene'] - df['bind_ene_teo']
-
-df = df[(df['Diferencia_bind_ene'] <= 500) & (df['Diferencia_bind_ene'] >= -500)]
+df['Diff_bind_ene'] = df['bind_ene'] - df['bind_ene_teo']
 
 df.to_csv('data_cleaned.csv', sep=';', index=False)
 print(df.head())
 
-
 plt.figure(figsize=(10, 6))
-scatter = plt.scatter(df['N'], df['Z'], c=df['Diferencia_bind_ene'], cmap='jet', edgecolor='None', s=7)
-
+scatter = plt.scatter(df['N'], df['Z'], c=df['Diff_bind_ene'], cmap='jet', edgecolor='None', s=25)
 cbar = plt.colorbar(scatter)
-
 plt.xlabel('N')
-plt.ylabel('Z')
-
+plt.ylabel('Z') 
 plt.grid()
 plt.savefig('bind_teoexp_dif.png')
 plt.show()
+
+#3D plot
+fig = plt.figure(figsize=(10, 6))
+ax = fig.add_subplot(111, projection='3d')  
+scatter = ax.scatter(df['Z'], df['N'], df['Diff_bind_ene'], c=df['Diff_bind_ene'], cmap='jet', edgecolor='None', s=25)
+ax.set_xlabel('Z')
+ax.set_ylabel('N')
+cbar = plt.colorbar(scatter, ax=ax)
+plt.savefig('bind_teoexp_dif_3D.png') 
+plt.show()
+
+
 
 
